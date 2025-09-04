8689e608b278e71d2f770ad18a82d5d5c7465b9f
Samuele Felici <130688236+Saolino91@users.noreply.github.com>
2025-09-04 13:21:45 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -2,17 +2,21 @@ ABSENCE_CODES = {
     "PC","AA","AF","AO","AP","AS","AM","PADm","PAD","PA",
     "CM8","CM","PD","FP","FU","F1/2","F","AGP","I4","I91","I",
     "MA3","MA20","MA","ES","FI","S","PSn","PSm3","PSm2","PAL",
-    "P626","PS30","PS","PZ","PE","PPR","PP","PVM","SG","NF" 
+    "P626","PS30","PS","PZ","PE","PPR","PP","PVM","SG","NF"
 }
 
 RESIDENZA_RENAME = {
     "CASTELFIDARDO": "C.FID.",
     "FILOTTRANO": "FILOT",
 }
-DEFAULT_SORT = ["Residenza","Categoria","Turno","Inizio"]  # editabile
+
 HEADER_PROBE = "Cognome e Nome"
+
 EXPECTED_COLUMNS = [
-    "Cognome e Nome","Matricola","Residenza",
-    "Turno","Inizio","Fine","Note"
+    "Cognome e Nome", "Matricola", "Residenza",
+    "Turno", "Inizio", "Fine", "Indennità e note"
 ]
+
+DEFAULT_SORT = ["Residenza", "Turno", "Inizio"]
+
 TITLE = "Servizio Giornaliero"
