14fed798a7f17eb35bda8e25d3d0ab26f64ee348
Samuele Felici <130688236+Saolino91@users.noreply.github.com>
2025-10-16 10:37:54 +0200

    Fix indentation for expander in app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ if uploaded:
 st.divider()
 
 # Opzioni di ordinamento e visualizzazione
-with st.expander("Opzioni di visualizzazione", expanded=True)
+with st.expander("Opzioni di visualizzazione", expanded=True):
     col1, col2 = st.columns([1,1])
     with col1:
         inner_sort_choice = st.radio(
